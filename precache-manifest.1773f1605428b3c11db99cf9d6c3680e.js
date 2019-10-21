e70f508841865d0a9c799f5bf2176b4737da8a3d
Andrey Kuts <andrey@Andreys-Mac.local>
2019-10-21 20:02:05 +0300

    Updates

---
+++ precache-manifest.1773f1605428b3c11db99cf9d6c3680e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e17cd418b870fc731cf0fc7f4d7d2af",
+    "url": "/real-estate/index.html"
+  },
+  {
+    "revision": "8339539eb30b8e26b304",
+    "url": "/real-estate/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69d2ebccb72f78ff32e4",
+    "url": "/real-estate/static/js/2.f51bb6ef.chunk.js"
+  },
+  {
+    "revision": "8339539eb30b8e26b304",
+    "url": "/real-estate/static/js/main.bc6b951a.chunk.js"
+  },
+  {
+    "revision": "47feba8da3af78e9a860",
+    "url": "/real-estate/static/js/runtime-main.eb8f814b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/real-estate/static/media/logo.25bf045c.svg"
+  }
+]);
